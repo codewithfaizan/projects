cedf2ec5b6cafc12a13974f2697b645f6e769ea9
codewithfaizan <faizanoic.com@gmail.com>
2022-09-13 02:03:00 +0000

    updates

--- tasky-application/server/app.js
+++ tasky-application/server/app.js
@@ -4,6 +4,10 @@ import express from "express";
 
 import fs from "fs/promises";
 
+import bcrypt from "bcrypt";
+
+import randomString from "../utils/randomString.js";
+// import { fstatSync } from "fs";
 // instatantiation - all express things are stored as class
 // all methods are trigerred inside the app.js that are in the express
 const app = express();
@@ -25,19 +29,13 @@ app.post("/api/signup", async (req, res) => {
     // console.log(req.body);
     let { firstname, lastname, email, password, password2, address, phone } =
       req.body;
-    let body = req.body;
-    // console.log(email);
-    if (
+    // let body = req.body;
 
-      !firstname ||
-      !lastname ||
-      !password ||
-      !password2 ||
-   
-      !email ||
-      !phone ||
-      !address 
-   
+
+
+    // console.log(email)
+    if (
+      !email || !firstname || !lastname || !phone || !address || !password || !password2
     ) {
       return res.status(400).json({ "error": "some fields are missing" });
     }
@@ -45,13 +43,34 @@ app.post("/api/signup", async (req, res) => {
       return res.status(400).json({ "error": "Password does not match" });
     }
 
+  
+    
+    // Check Duplication of Email & Mobile
     let fileData = await fs.readFile("data.json");
     fileData = JSON.parse(fileData);
 
-    fileData.push(body);
-    await fs.writeFile("data.json", JSON.stringfy(fileData));
+    let emailFound = fileData.find((ele)=> ele.email == email)
+    console.log(emailFound)
+    // if(emailFound){
+    //   return res.status(409)
+    // }
+    
+    password = await bcrypt.hash(password, 12);
+
+    // Generate a 12 Digit Rnadom String for user_id
+    let user_id = randomString(16);
+    console.log(user_id);
+
+    let userData = { user_id, filename, lastname, email, password, address}
+    userData.tasks=[];
+    // fileData.push(body);
+    // await fs.writeFile("data.json", JSON.stringfy(fileData));
+    fileData.push(userData);
+    await fs.writeFile("data.json", JSON.stringify(fileData));
     res.status(200).json({ success: "Sign Up Successfull" });
-  } catch (error) {
+  } 
+  
+    catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
---
+++ tasky-application/utils/randomString.js
@@ -0,0 +1,16 @@
+// randomString generation of given length 
+
+function randomString(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+// console.log(randomString(12));
+
+export default randomString;
